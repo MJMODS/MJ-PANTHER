79ec99be0391ab36342af8b73c3dbc4f5045a281
MJMODS <95528082+MJMODS@users.noreply.github.com>
2022-01-24 16:08:59 +0530

    Create antiViewOnce.js

---
+++ plugins/antiViewOnce.js
@@ -0,0 +1,27 @@
+const Asena = require("../Utilis/events");
+
+const { MessageType } = require("@adiwajshing/baileys");
+
+Asena.addCommand(
+
+  { pattern: "vv", fromMe: true, desc: "Anti viewOnce" },
+
+  async (message, match) => {
+
+    if (message.reply_message.image || message.reply_message.video) {
+
+      return await message.sendMessage(
+
+        await message.reply_message.downloadMediaMessage(),
+
+        { quoted: message.quoted },
+
+        message.reply_message.image ? MessageType.image : MessageType.video
+
+      )
+
+    }
+
+  }
+
+)
